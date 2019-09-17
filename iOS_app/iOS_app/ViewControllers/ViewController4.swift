62d5db386a60b8516417c64c6501d7cd0d65861e
Bruno <brunomilliet89@gmail.com>
2019-09-17 19:35:18 -0300

    controller

---
+++ iOS_app/iOS_app/ViewControllers/ViewController4.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ViewController4: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    //label.text = "iOS native view 4"
+    view.backgroundColor = .orange
+  }
+}
