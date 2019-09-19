b068a2b91c480af83d5cb74d9b39237492652d04
Bruno <brunomilliet89@gmail.com>
2019-09-19 19:02:09 -0300

    view

--- flutter_app/lib/views/noninitial_views/view_four.dart
+++ flutter_app/lib/views/noninitial_views/view_four.dart
@@ -9,13 +9,33 @@ class _ViewFourState extends State<ViewFour> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: Container(),
+      body: _body(),
     );
   }
 
   AppBar _appBar() {
     return AppBar(
       title: Text("View Four"),
+    );
+  }
+
+  Widget _body() {
+    return Column(
+      children: <Widget>[_imagePicker(), Container(height: 15), _camera()],
+    );
+  }
+
+  FlatButton _imagePicker() {
+    return FlatButton(
+      child: Text("image"),
+      onPressed: () {},
+    );
+  }
+
+  FlatButton _camera() {
+    return FlatButton(
+      child: Text("camera"),
+      onPressed: () {},
     );
   }
 }
